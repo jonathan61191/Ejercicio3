54cdb9f5b64d96974f2084b1f8ea5b0d33f40a62
Jonathan Garcia <jonatan.61191@hotmail.com>
2023-08-29 21:05:38 -0500

    comit1

---
+++ ejercicio3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+//Un vendedor recibe un sueldo base más un 10% extra por comisión de sus ventas, el vendedor desea saber cuánto
+//dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes y el total que recibirá
+//en el mes tomando en cuenta su sueldo base y comisiones
+
+namespace Ejercicio3
+{
+    public class Program
+{
+    static void Main(string[] args)
+    {
+            //Declaracion de variables
+
+            decimal sueldoBase, venta1, venta2, venta3, salarioTotal, comision;
+
+            //solicitar variables por pantalla
+
+            Console.WriteLine("Ingrese su sueldo Base: ");
+            sueldoBase = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su Venta1: ");
+            venta1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su Venta2: ");
+            venta2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su Venta3: ");
+            venta3 = Convert.ToDecimal(Console.ReadLine());
+
+
+
+
+
+
+        }
+}
+}
+
